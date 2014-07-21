8a2ef17b407f96e894d67db5c73762d0d0b5102c
Dalton Cherry <daltoniam@gmail.com>
2014-07-21 16:44:15 -0500

    almost finished

--- Websocket.swift
+++ Websocket.swift
@@ -7,15 +7,42 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 import Foundation
+import CoreFoundation
 
 protocol WebsocketDelegate {
     func websocketDidConnect()
     func websocketDidDisconnect(error: NSError?)
+    func websocketDidWriteError(error: NSError?)
+    func websocketDidReceiveMessage(text: String)
+    func websocketDidReceiveData(data: NSData)
 }
 
 class Websocket : NSObject, NSStreamDelegate {
     
-    //Constant Header Values.
+    enum OpCode : UInt8 {
+        case ContinueFrame = 0x0
+        case TextFrame = 0x1
+        case BinaryFrame = 0x2
+        //3-7 are reserved.
+        case ConnectionClose = 0x8
+        case Ping = 0x9
+        case Pong = 0xA
+        //B-F reserved.
+    }
+    
+    enum CloseCode : UInt16 {
+        case Normal                 = 1000
+        case GoingAway              = 1001
+        case ProtocolError          = 1002
+        case ProtocolUnhandledType  = 1003
+        // 1004 reserved.
+        case NoStatusReceived       = 1005
+        //1006 reserved.
+        case Encoding               = 1007
+        case PolicyViolated         = 1008
+        case MessageTooBig          = 1009
+    }
+    //Constant Values.
     let headerWSUpgradeName     = "Upgrade"
     let headerWSUpgradeValue    = "websocket"
     let headerWSHostName        = "Host"
@@ -28,9 +55,21 @@ class Websocket : NSObject, NSStreamDelegate {
     let headerWSKeyName         = "Sec-WebSocket-Key"
     let headerOriginName        = "Origin"
     let headerWSAcceptName      = "Sec-WebSocket-Accept"
+    let BUFFER_MAX              = 2048
+    let FinMask: UInt8          = 0x80
+    let OpCodeMask: UInt8       = 0x0F
+    let RSVMask: UInt8          = 0x70
+    let MaskMask: UInt8         = 0x80
+    let PayloadLenMask: UInt8   = 0x7F
+    let MaxFrameSize: Int       = 32
     
-    //Class Constants
-    let BUFFER_MAX = 2048
+    class WSResponse {
+        var isFin = false
+        var code: OpCode = .ContinueFrame
+        var bytesLeft = 0
+        var frameCount = 0
+        var buffer: NSMutableData?
+    }
     
     var delegate: WebsocketDelegate?
     var _url: NSURL
@@ -39,7 +78,7 @@ class Websocket : NSObject, NSStreamDelegate {
     var _isRunLoop = false
     var _isConnected = false
     var _writeQueue: NSOperationQueue?
-    var _readStack = Array<String>()
+    var _readStack = Array<WSResponse>()
     var _inputQueue = Array<NSData>()
     var _fragBuffer: NSData?
     
@@ -56,17 +95,17 @@ class Websocket : NSObject, NSStreamDelegate {
     
     ///disconnect from the websocket server
     func disconnect() {
-        
+        writeError(CloseCode.Normal.toRaw())
     }
     
     ///write a string to the websocket. This sends it as a text frame.
     func writeString(str: String) {
-        
+        dequeueWrite(str.dataUsingEncoding(NSUTF8StringEncoding)!, code: .TextFrame)
     }
     
     ///write binary data to the websocket. This sends it as a binary frame.
     func writeData(data: NSData) {
-        
+        dequeueWrite(data, code: .BinaryFrame)
     }
     
     //private methods below!
@@ -129,7 +168,7 @@ class Websocket : NSObject, NSStreamDelegate {
         
         if eventCode == .HasBytesAvailable {
             if(aStream == _inputStream) {
-                
+                processInputStream()
             }
         } else if eventCode == .ErrorOccurred {
             disconnectStream(aStream!.streamError)
@@ -196,7 +235,7 @@ class Websocket : NSObject, NSStreamDelegate {
             }
             let buffer = UnsafePointer<UInt8>(work.bytes)
             processRawMessage(buffer, bufferLen: work.length)
-            _inputQueue = _inputQueue.filter{$0 != data }
+            _inputQueue = _inputQueue.filter{$0 != data}
             dequeueInput()
         }
     }
@@ -251,7 +290,76 @@ class Websocket : NSObject, NSStreamDelegate {
     
     ///process the websocket data
     func processRawMessage(buffer: UnsafePointer<UInt8>, bufferLen: Int) {
-        
+        if _readStack.count > 0 {
+            if bufferLen < 2 {
+                _fragBuffer = NSData(bytes: buffer, length: bufferLen)
+                return
+            }
+            var response = _readStack[_readStack.count-1]
+            if response.bytesLeft > 0 {
+                var len = response.bytesLeft
+                var extra = bufferLen - response.bytesLeft
+                if response.bytesLeft > bufferLen {
+                    len = bufferLen
+                    extra = 0
+                }
+                response.bytesLeft -= len
+                response.buffer?.appendData(NSData(bytes: buffer, length: len))
+                processResponse(response)
+                var offset = bufferLen - extra
+                if extra > 0 {
+                    processExtra((buffer+offset), bufferLen: extra)
+                }
+                return
+            } else {
+                let isFin = (FinMask & buffer[0])
+                let receivedOpcode = (OpCodeMask & buffer[0])
+                let isMasked = (MaskMask & buffer[1])
+                let payloadLen = (PayloadLenMask & buffer[1])
+                var offset = 2
+                if((isMasked > 0 || (RSVMask & buffer[0]) > 0) && receivedOpcode != OpCode.Pong.toRaw()) {
+                    
+                }
+            }
+        }
+    }
+    
+    ///process the extra of a buffer
+    func processExtra(buffer: UnsafePointer<UInt8>, bufferLen: Int) {
+        if bufferLen < 2 {
+            _fragBuffer = NSData(bytes: buffer, length: bufferLen)
+        } else {
+            processRawMessage(buffer, bufferLen: bufferLen)
+        }
+    }
+    
+    ///process the finished response of a buffer
+    func processResponse(response: WSResponse) -> Bool {
+        if response.isFin && response.bytesLeft <= 0 {
+            if response.code == .Ping {
+                let data = response.buffer! //local copy so it is perverse for writing
+                dequeueWrite(data, code: OpCode.Pong)
+            } else if response.code == .TextFrame {
+                var str: NSString? = NSString(data: response.buffer!, encoding: NSUTF8StringEncoding)
+                if !str {
+                    writeError(CloseCode.Encoding.toRaw())
+                    return false
+                }
+                dispatch_async(dispatch_get_main_queue(),{
+                    self.workaroundMethod()
+                    self.delegate?.websocketDidReceiveMessage(str!)
+                    })
+            } else if response.code == .BinaryFrame {
+                let data = response.buffer! //local copy so it is perverse for writing
+                dispatch_async(dispatch_get_main_queue(),{
+                    self.workaroundMethod()
+                    self.delegate?.websocketDidReceiveData(data)
+                    })
+            }
+            _readStack.removeLast()
+            return true
+        }
+        return false
     }
     
     ///Create an error
@@ -259,6 +367,68 @@ class Websocket : NSObject, NSStreamDelegate {
         var details = Dictionary<String,String>()
         details[NSLocalizedDescriptionKey] =  detail
         return NSError(domain: "Websocket", code: code, userInfo: details)
+    }
+    
+    ///write a an error to the socket
+    func writeError(code: UInt16) {
+        var buffer = UnsafePointer<UInt16>(sizeof(UInt16))
+        buffer[0] = code.byteSwapped
+        dequeueWrite(NSData(bytes: buffer, length: sizeof(UInt16)), code: .ConnectionClose)
+    }
+    ///used to write things to the stream in a
+    func dequeueWrite(data: NSData, code: OpCode) {
+        if !_writeQueue {
+            _writeQueue = NSOperationQueue()
+            _writeQueue!.maxConcurrentOperationCount = 1
+        }
+        _writeQueue!.addOperationWithBlock {
+            var offset = 2
+            let bytes = UnsafePointer<UInt8>(data.bytes)
+            let dataLength = data.length
+            let frame = NSMutableData(capacity: dataLength + self.MaxFrameSize)
+            let buffer = UnsafePointer<UInt8>(frame.mutableBytes)
+            buffer[0] = self.FinMask | code.toRaw()
+            if dataLength < 126 {
+                buffer[1] = CUnsignedChar(dataLength)
+            } else if dataLength <= Int(Int16.max) {
+                buffer[1] = 126
+                var sizeBuffer = UnsafePointer<UInt16>((buffer+offset))
+                sizeBuffer[0] = UInt16(dataLength).byteSwapped
+                offset += sizeof(UInt16)
+            } else {
+                buffer[1] = 127
+                var sizeBuffer = UnsafePointer<UInt64>((buffer+offset))
+                sizeBuffer[0] = UInt64(dataLength).byteSwapped
+                offset += sizeof(UInt64)
+            }
+            buffer[1] |= self.MaskMask
+            var maskKey = UnsafePointer<UInt8>(buffer + offset)
+            SecRandomCopyBytes(kSecRandomDefault, UInt(sizeof(UInt32)), maskKey)
+            offset += sizeof(UInt32)
+            
+            for (var i = 0; i < dataLength; i++) {
+                buffer[offset] = bytes[i] ^ maskKey[i % sizeof(UInt32)]
+                offset += 1
+            }
+            var total = 0
+            while true {
+                if !self._outputStream {
+                    break
+                }
+                let writeBuffer = UnsafePointer<UInt8>(frame.bytes+total)
+                var len = self._outputStream!.write(writeBuffer, maxLength: offset-total)
+                if len < 0 {
+                    self.delegate?.websocketDidDisconnect(self._outputStream!.streamError)
+                    break
+                } else {
+                    total += len
+                }
+                if total >= offset {
+                    break
+                }
+            }
+            
+            }
     }
     
 }
