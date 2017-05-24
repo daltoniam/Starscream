ccc9c4ea432f8eb2d5b7826322f1ff660785500b
Joseph Ross <joe@junelife.com>
2017-05-24 07:37:51 -0700

    Add compression classes and unit tests.

---
+++ Source/Compression.swift
@@ -0,0 +1,199 @@
+//
+//  Compression.swift
+//  Starscream
+//
+//  Created by Joseph Ross on 5/23/17.
+//  Copyright © 2017 Vluxe. All rights reserved.
+//
+
+import Foundation
+
+private let ZLIB_VERSION = Array("1.2.8".utf8CString)
+
+private let Z_OK:CInt = 0
+private let Z_BUF_ERROR:CInt = -5
+
+private let Z_SYNC_FLUSH:CInt = 2
+
+class Decompressor {
+    var strm = z_stream()
+    var buffer = [UInt8](repeating: 0, count: 0x2000)
+    var inflateInitialized = false
+    let windowBits:Int
+    
+    init?(windowBits:Int) {
+        self.windowBits = windowBits
+        guard initInflate() else { return nil }
+    }
+    
+    private func initInflate() -> Bool {
+        if Z_OK == inflateInit2(strm: &strm, windowBits: -CInt(windowBits),
+                                version: ZLIB_VERSION, streamSize: CInt(MemoryLayout<z_stream>.size))
+        {
+            inflateInitialized = true
+            return true
+        }
+        return false
+    }
+    
+    func reset() throws {
+        teardownInflate()
+        guard initInflate() else { throw NSError() }
+    }
+    
+    func decompress(_ data: Data) throws -> Data {
+        let data = data
+        let tail = Data([0x00, 0x00, 0xFF, 0xFF])
+        
+        var decompressed = Data()
+        
+        try decompress(in: data, out: &decompressed)
+        try decompress(in: tail, out: &decompressed)
+        
+        return decompressed
+        
+    }
+    
+    private func decompress(in data: Data, out:inout Data) throws {
+        var res:CInt = 0
+        data.withUnsafeBytes { (ptr:UnsafePointer<UInt8>) -> Void in
+            strm.next_in = ptr
+            strm.avail_in = CUnsignedInt(data.count)
+            
+            repeat {
+                strm.next_out = UnsafeMutablePointer<UInt8>(&buffer)
+                strm.avail_out = CUnsignedInt(buffer.count)
+                
+                res = inflate(strm: &strm, flush: 0)
+                
+                let byteCount = buffer.count - Int(strm.avail_out)
+                out.append(buffer, count: byteCount)
+            }
+                while res == Z_OK && strm.avail_out == 0
+            
+        }
+        guard (res == Z_OK && strm.avail_out > 0)
+            || (res == Z_BUF_ERROR && Int(strm.avail_out) == buffer.count)
+            else {
+                throw NSError()//"Error during inflate: \(res)")
+        }
+    }
+    
+    private func teardownInflate() {
+        if inflateInitialized, Z_OK == inflateEnd(strm: &strm) {
+            inflateInitialized = false
+        }
+    }
+    
+    deinit {
+        teardownInflate()
+    }
+    
+    @_silgen_name("inflateInit2_") func inflateInit2(strm: UnsafeMutableRawPointer, windowBits: CInt,
+                                                    version: UnsafePointer<CChar>, streamSize: CInt) -> CInt
+    @_silgen_name("inflate") func inflate(strm: UnsafeMutableRawPointer, flush: CInt) -> CInt
+    @discardableResult
+    @_silgen_name("inflateEnd") func inflateEnd(strm: UnsafeMutableRawPointer) -> CInt
+}
+
+class Compressor {
+    var strm = z_stream()
+    var buffer = [UInt8](repeating: 0, count: 0x2000)
+    var deflateInitialized = false
+    let windowBits:Int
+    
+    init?(windowBits: Int) {
+        self.windowBits = windowBits
+        guard initDeflate() else { return nil }
+    }
+    
+    private func initDeflate() -> Bool {
+        if Z_OK == deflateInit2(strm: &strm, level: Z_DEFAULT_COMPRESSION, method: Z_DEFLATED,
+                                windowBits: -CInt(windowBits), memLevel: 8, strategy: Z_DEFAULT_STRATEGY,
+                                version: ZLIB_VERSION, streamSize: CInt(MemoryLayout<z_stream>.size))
+        {
+            deflateInitialized = true
+            return true
+        }
+        return false
+    }
+    
+    func reset() throws {
+        teardownDeflate()
+        guard initDeflate() else { throw NSError() }
+    }
+    
+    func compress(_ data: Data) throws -> Data {
+        var compressed = Data()
+        var res:CInt = 0
+        data.withUnsafeBytes { (ptr:UnsafePointer<UInt8>) -> Void in
+            strm.next_in = ptr
+            strm.avail_in = CUnsignedInt(data.count)
+            
+            repeat {
+                strm.next_out = UnsafeMutablePointer<UInt8>(&buffer)
+                strm.avail_out = CUnsignedInt(buffer.count)
+                
+                res = deflate(strm: &strm, flush: Z_SYNC_FLUSH)
+                
+                let byteCount = buffer.count - Int(strm.avail_out)
+                compressed.append(buffer, count: byteCount)
+            }
+            while res == Z_OK && strm.avail_out == 0
+                
+        }
+        
+        guard res == Z_OK && strm.avail_out > 0
+            || (res == Z_BUF_ERROR && Int(strm.avail_out) == buffer.count)
+        else {
+            NSLog("Error during deflate: \(res)")
+            throw NSError()
+        }
+        
+        compressed.removeLast(4)
+        return compressed
+    }
+    
+    private func teardownDeflate() {
+        if deflateInitialized, Z_OK == deflateEnd(strm: &strm) {
+            deflateInitialized = false
+        }
+    }
+    
+    deinit {
+        teardownDeflate()
+    }
+    
+    @_silgen_name("deflateInit2_") func deflateInit2(strm: UnsafeMutableRawPointer, level: CInt, method: CInt,
+                                                     windowBits: CInt, memLevel: CInt, strategy: CInt,
+                                                     version: UnsafePointer<CChar>, streamSize: CInt) -> CInt
+    @_silgen_name("deflate") func deflate(strm: UnsafeMutableRawPointer, flush: CInt) -> CInt
+    @discardableResult
+    @_silgen_name("deflateEnd") func deflateEnd(strm: UnsafeMutableRawPointer) -> CInt
+    
+    private let Z_DEFAULT_COMPRESSION:CInt = -1
+    private let Z_DEFLATED:CInt = 8
+    private let Z_DEFAULT_STRATEGY:CInt = 0
+}
+
+struct z_stream {
+    var next_in: UnsafePointer<UInt8>? = nil            /* next input byte */
+    var avail_in: CUnsignedInt = 0                      /* number of bytes available at next_in */
+    var total_in: CUnsignedLong = 0                     /* total number of input bytes read so far */
+    
+    var next_out: UnsafeMutablePointer<UInt8>? = nil    /* next output byte should be put there */
+    var avail_out: CUnsignedInt = 0                     /* remaining free space at next_out */
+    var total_out: CUnsignedLong = 0                    /* total number of bytes output so far */
+    
+    var msg: UnsafePointer<CChar>? = nil                /* last error message, NULL if no error */
+    private var state: OpaquePointer? = nil             /* not visible by applications */
+    
+    private var zalloc: OpaquePointer? = nil            /* used to allocate the internal state */
+    private var zfree: OpaquePointer? = nil             /* used to free the internal state */
+    private var opaque: OpaquePointer? = nil            /* private data object passed to zalloc and zfree */
+    
+    var data_type: CInt = 0                             /* best guess about the data type: binary or text */
+    var adler: CUnsignedLong = 0                        /* adler32 value of the uncompressed data */
+    private var reserved: CUnsignedLong = 0             /* reserved for future use */
+}
+
---
+++ Tests/CompressionTests.swift
@@ -0,0 +1,52 @@
+//
+//  CompressionTests.swift
+//  Starscream
+//
+//  Created by Joseph Ross on 5/23/17.
+//  Copyright © 2017 Vluxe. All rights reserved.
+//
+
+import XCTest
+
+class CompressionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testBasic() {
+        let compressor = Compressor(windowBits: 15)!
+        let decompressor = Decompressor(windowBits: 15)!
+        
+        let rawData = "Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!".data(using: .utf8)!
+        
+        let compressed = try! compressor.compress(rawData)
+        let uncompressed = try! decompressor.decompress(compressed)
+        
+        XCTAssert(rawData == uncompressed)
+    }
+    
+    func testHugeData() {
+        let compressor = Compressor(windowBits: 15)!
+        let decompressor = Decompressor(windowBits: 15)!
+        
+        // 2 Gigs!
+//        var rawData = Data(repeating: 0, count: 0x80000000)
+        var rawData = Data(repeating: 0, count: 0x80000)
+        rawData.withUnsafeMutableBytes { (ptr: UnsafeMutablePointer<UInt8>) -> Void in
+            arc4random_buf(ptr, rawData.count)
+        }
+        
+        let compressed = try! compressor.compress(rawData)
+        let uncompressed = try! decompressor.decompress(compressed)
+        
+        XCTAssert(rawData == uncompressed)
+    }
+    
+}
