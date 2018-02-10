5ffb5731fdad42be3bb26c452ad55a0f6f75ad2d
Vai Kong Edward Li <vk.edward.li@gmail.com>
2018-02-11 06:17:20 +0800

    This is fix the absolute path defined in Starscream.modulemap and remove the module.modulemap file during Cocoapods installation

---
+++ zlib/injectXcodePath.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+defaultXcodePath="/Applications/Xcode.app/Contents/Developer"
+realXcodePath="`xcode-select -p`"
+
+fatal() {
+echo "[fatal] $1" 1>&2
+exit 1
+}
+
+absPath() {
+case "$1" in
+/*)
+printf "%s\n" "$1"
+;;
+*)
+printf "%s\n" "$PWD/$1"
+;;
+esac;
+}
+
+scriptDir="`dirname $0`"
+scriptName="`basename $0`"
+absScriptDir="`cd $scriptDir; pwd`"
+
+main() {
+for f in `find ${absScriptDir} -name Starscream.modulemap`; do
+cat ${f} | sed "s,${defaultXcodePath},${realXcodePath},g" > ${f}.new || fatal "Failed to update modulemap ${f}"
+mv ${f}.new ${f} || fatal "Failed to replace modulemap ${f}"
+done
+cd $(dirname $0)
+rm -f */module.modulemap
+}
+
+main $*
