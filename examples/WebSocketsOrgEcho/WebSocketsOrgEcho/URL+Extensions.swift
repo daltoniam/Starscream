fd8a3fc3fa8bcf6620724b0f4282d3204dd0443f
Kristaps Grinbergs <fassko@gmail.com>
2018-10-08 17:59:34 +0300

    Clean up example project

--- examples/WebSocketsOrgEcho/WebSocketsOrgEcho/URL+Extensions.swift
+++ examples/WebSocketsOrgEcho/WebSocketsOrgEcho/URL+Extensions.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 extension URL {
-  init(staticString string: StaticString) {
-    guard let url = URL(string: "\(string)") else {
-      preconditionFailure("Invalid static URL string: \(string)")
+    init(staticString string: StaticString) {
+        guard let url = URL(string: "\(string)") else {
+            preconditionFailure("Invalid static URL string: \(string)")
+        }
+        
+        self = url
     }
-    
-    self = url
-  }
 }
--- examples/WebSocketsOrgEcho/WebSocketsOrgEcho/ViewController.swift
+++ examples/WebSocketsOrgEcho/WebSocketsOrgEcho/ViewController.swift
@@ -40,4 +40,3 @@ class ViewController: UIViewController, WebSocketDelegate {
         socket.connect()
     }
 }
-
