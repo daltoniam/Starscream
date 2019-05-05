a2ed45c0b2f996cb8c335c4f270ecc68c3bd4c0f
Kristaps Grinbergs <fassko@gmail.com>
2019-05-05 17:42:46 +0300

    Update gems and allow warnings for now

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -o pipefail && xcodebuild -project Starscream.xcodeproj -scheme Starscream CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build | xcpretty
 swift build
 pod repo update
-pod lib lint --verbose
+pod lib lint --verbose --allow-warnings
