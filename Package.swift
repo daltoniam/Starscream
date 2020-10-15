fd9a90cc18e33fd5d5b06aeda9f317134eed572f
Yehor Popovych <yehor@crossroad.io>
2020-10-15 19:25:39 +0300

    attempt to fix linux build

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 
 //
 //  Package.Swift
@@ -29,11 +29,12 @@ let package = Package(
         ],
         dependencies: [],
         targets: [
-            .target(name: "Starscream",
-                    path: "Sources")
+            .target(
+                name: "Starscream",
+                path: "Sources",
+                linkerSettings: [
+                    .linkedLibrary("z")
+                ]
+            )
         ]
 )
-
-#if os(Linux)
-    package.dependencies.append(.package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0"))
-#endif
